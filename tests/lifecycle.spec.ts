2b9d14ccd281e51d27cddc48e743a481b4c970cc
EduardoRFS <theeduardorfs@gmail.com>
2019-08-04 20:28:03 -0300

    one more use case

--- tests/lifecycle.spec.ts
+++ tests/lifecycle.spec.ts
@@ -31,10 +31,8 @@ withContext('after each', { foo: 0 }, ({ test, afterEach }) => {
 withContext<{ foo: number }>(
   'before each without context',
   ({ test, beforeEach }) => {
-    let fooInternal = 0;
-    beforeEach(() => {
-      fooInternal++;
-      return { foo: fooInternal };
+    beforeEach((state = { foo: 0 }) => {
+      return { foo: state.foo + 1 };
     });
     test('foo 1', ({ foo }) => {
       expect(foo).toBe(1);
